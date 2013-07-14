8788e9c952fbd8e5909908c148c2dc1b22d91399
sjtufs <sjtu_fs@outlook.com>
2013-07-14 16:21:33 +0800

    boring

--- awesome-wm/rc.lua
+++ awesome-wm/rc.lua
@@ -247,9 +247,9 @@ globalkeys = awful.util.table.join(
     awful.key({ modkey,           }, "Right",  awful.tag.viewnext       ),
     awful.key({ modkey,           }, "Escape", awful.tag.history.restore),
     awful.key({ }, "XF86AudioRaiseVolume", function ()
-       awful.util.spawn("amixer set Master 5%+") end),
+       awful.util.spawn("amixer set Master 2%+") end),
     awful.key({ }, "XF86AudioLowerVolume", function ()
-       awful.util.spawn("amixer set Master 5%-") end),
+       awful.util.spawn("amixer set Master 2%-") end),
     awful.key({ }, "XF86AudioMute", function ()
        awful.util.spawn("amixer sset Master toggle") end),
     awful.key({ modkey,           }, "j",
