50f7b381b76efe87037054d91f1b613fd08011a8
sjtufs <sjtu_fs@outlook.com>
2013-07-31 21:57:44 +0800

    hahaha

--- awesome-wm/rc.lua
+++ awesome-wm/rc.lua
@@ -18,7 +18,7 @@ require("debian.menu")
 beautiful.init("/usr/share/awesome/themes/bamboo/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "konsole"
+terminal = "xfce4-terminal"
 --editor = os.getenv("EDITOR") or "editor"
 editor = "gvim"
 editor_cmd = terminal .. " -e " .. editor
