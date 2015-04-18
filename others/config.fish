bf53ee3ecbd67506ea2f3134dd6efe42c998a47f
ihsgnef <sjtu_fs@Outlook.com>
2015-04-19 01:52:09 +0800

    Update

--- others/config.fish
+++ others/config.fish
@@ -6,8 +6,8 @@ tmux
 set -g -x PATH /usr/local/bin /Users/fs/cross-compiler $PATH
 set fish_greeting ""
 alias vmrun='/Applications/VMware\ Fusion.app/Contents/Library/vmrun'
-alias vmubuntu='vmrun -T fusion start ~/Documents/Virtual\ Machines.localized/Ubuntu\ 64-bit.vmwarevm/Ubuntu\ 64-bit.vmx nogui'
-alias ubuntu='ssh fs@172.16.145.184'
+alias vmubuntu='vmrun -T fusion start ~/Documents/Virtual\ Machines.localized/Ubuntu\ 64-bit\ 14.04.2.vmwarevm/Ubuntu\ 64-bit\ 14.04.2.vmx nogui'
+alias ubuntu='ssh fs@172.16.145.198'
 
 alias antlr4='java -jar /usr/local/lib/antlr-4.0-complete.jar'
 alias grun='java org.antlr.v4.runtime.misc.TestRig'
@@ -26,13 +26,9 @@ alias py='python'
 alias tt='tmux attach -t'
 alias tl='tmux list-sessions'
 
-alias eclim='nohup /Applications/eclipse\ 2/eclimd -Dosgi.instance.area.default=/Users/fs/workspace &'
-alias eclimshutdown='/Applications/eclipse\ 2/eclim -command shutdown'
-
-alias hadoop87='ssh kddcup@172.16.7.87'
-alias hadoop88='ssh kddcup@172.16.7.88'
-alias tqchen='ssh -X tqchen@172.16.2.17'
 alias galaxy='ssh fs@adapt.seiee.sjtu.edu.cn'
+
+alias gup='git add --all; git commit -a -m "Update"; git push origin master'
 
 setenv EDITOR vim
 
