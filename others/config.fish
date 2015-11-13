d2dc3aa77b63067a843d6674a36bc702a552438e
sjtufs <sjtu_fs@outlook.com>
2015-11-13 22:22:22 +0800

    update

--- others/config.fish
+++ others/config.fish
@@ -1,15 +1,15 @@
-
-# tmux
+tmux
 
 set -g -x PATH /usr/local/bin /Users/fs/cross-compiler $PATH
 set fish_greeting ""
 setenv EDITOR vim
 
-alias nnn='ncmpc'
+alias nnn='ncmpcpp'
 alias grep='grep --color=auto'
 alias cx='cd ..'
 alias l='ls -FsGgHhlp'
-alias vi='vim'
+alias vim='mvim -v'
+alias vi='mvim -v'
 alias irc='irssi'
 alias clk='tty-clock -sctDC 1'
 alias awd='ls -lghGFH'
@@ -23,9 +23,19 @@ alias milky='ssh -p 9022 fs@202.120.38.145'
 alias gup='git add --all :/; git commit -a -m "Update"; git push origin master'
 alias py8='env OMP_NUM_THREADS=8 python'
 alias py4='env OMP_NUM_THREADS=4 python'
+alias htop='sudo htop -u fs'
+
+alias gcm='git commit'
+alias gst='git status -s'
+alias gco='git checkout'
 
 set --export PYTHONPATH /Library/Python/2.7/site-packages/
-set PATH /Users/$USER/.opam/system/bin $PATH
 
-# OPAM configuration
-. /Users/fs/.opam/opam-init/init.fish > /dev/null 2> /dev/null or true
+eval (python -m virtualfish auto_activation)
+
+begin
+    set --local AUTOJUMP_PATH $HOME/.autojump/share/autojump/autojump.fish
+    if test -e $AUTOJUMP_PATH
+        source $AUTOJUMP_PATH
+    end
+end
