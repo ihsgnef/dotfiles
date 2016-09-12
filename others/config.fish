151bed2e4e516ad668dc22c7f44ced723df44d9c
sjtufs <sjtu_fs@outlook.com>
2016-09-11 22:54:17 -0400

    Update

--- others/config.fish
+++ others/config.fish
@@ -1,4 +1,4 @@
-tmux
+# tmux
 
 set -g -x PATH /usr/local/bin /Users/fs/cross-compiler $PATH
 set fish_greeting ""
@@ -15,15 +15,21 @@ alias clk='tty-clock -sctDC 1'
 alias awd='ls -lghGFH'
 alias asd='cd'
 alias rg='ranger'
-alias py='python'
 alias tt='tmux attach -t'
 alias tl='tmux list-sessions'
 alias galaxy='ssh fs@adapt.seiee.sjtu.edu.cn'
 alias milky='ssh -p 9022 fs@202.120.38.145'
 alias gup='git add --all :/; git commit -a -m "Update"; git push origin master'
+alias py16='env OMP_NUM_THREADS=16 python'
 alias py8='env OMP_NUM_THREADS=8 python'
 alias py4='env OMP_NUM_THREADS=4 python'
+alias py2='env OMP_NUM_THREADS=2 python'
+alias py='env OMP_NUM_THREADS=1 python'
 alias htop='sudo htop -u fs'
+alias conda='/Users/fs/miniconda2/bin/conda'
+alias chromium='/Applications/Chromium.app/Contents/MacOS/Chromium --ppapi-flash-path=/Library/Internet\ Plug-Ins/PepperFlashPlayer/PepperFlashPlayer.plugin --ppapi-flash-version=21.0.0.197'
+alias xxxshow='setfile -a v ~/Downloads/untitled.dmg'
+alias xxxhide='setfile -a V ~/Downloads/untitled.dmg'
 
 set --export PYTHONPATH /Library/Python/2.7/site-packages/
 
