671f1640755f447e28d120f61758f47295041599
ihsgnef <shifeng@cs.umd.edu>
2018-02-17 22:16:01 -0500

    update

--- setup_thinkpad.sh
+++ setup_thinkpad.sh
@@ -1,8 +1,6 @@
-cd Downloads
+cd ~/Downloads
 sudo apt update
-sudo apt install build-essential vim git tmux fish awesome arandr
-sudo apt install ibus-rime ibus-gtk ibus-gtk3 ibus-qt4
-sudo apt install gnome-terminal thunar
+sudo apt install build-essential vim git tmux fish awesome arandr ibus-rime ibus-gtk ibus-gtk3 ibus-qt4 gnome-terminal thunar software-properties-common python-software-properties
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 sudo apt-get -f install
@@ -11,7 +9,7 @@ mkdir -p ~/.vim/colors
 cd ~/.vim/colors
 git clone git://github.com/chriskempson/base16-vim.git base16
 cp base16/colors/*.vim .
-cd Downloads
+cd ~/Downloads
 curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > install.sh
 sh ./install.sh
 sudo add-apt-repository ppa:linrunner/tlp
@@ -22,6 +20,9 @@ sudo apt install acpitool
 sudo tlp start
 sudo apt install powertop
 sudo powertop --auto-tune
+sudo add-apt-repository  ppa:klaus-vormweg/awesome -y
+sudo apt update
+sudo apt install  awesome -y
 git config --global user.email "shifeng@cs.umd.edu"
 git config --global user.name "ihsgnef"
 git config --global alias.co checkout
