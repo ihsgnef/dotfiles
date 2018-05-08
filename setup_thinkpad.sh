ab56a4d7964fc776e1a82b3829153788fd1fb167
ihsgnef <shifeng@cs.umd.edu>
2018-05-08 11:37:29 -0400

    up

--- setup_thinkpad.sh
+++ setup_thinkpad.sh
@@ -42,6 +42,7 @@ git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci commit
 git config --global alias.st status
+git config --global push.default simple
 
 cp ~/dotfiles/vimrc ~/.vimrc
 cp ~/dotfiles/tmux.conf ~/.tmux.conf
