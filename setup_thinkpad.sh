30b32b0d115963663c3b3559b3e4a90e3a909fb0
ihsgnef <shifeng@cs.umd.edu>
2020-02-25 21:07:53 -0500

    update

--- setup_thinkpad.sh
+++ setup_thinkpad.sh
@@ -1,51 +1,54 @@
-sudo add-apt-repository  ppa:klaus-vormweg/awesome -y
-sudo apt-add-repository ppa:fish-shell/release-2
 sudo apt update
-sudo apt install build-essential vim git curl tmux xclip \
-software-properties-common python-software-properties arandr ranger \
-thunar tlp vlc acpid rofi mupdf gnome-screensaver gnome-terminal \
-ibus-rime ibus-gtk ibus-gtk3 ibus-qt4 libx11-dev python-dev \
-libxft-dev lxappearance lxrandr sshfs fish awesome xscreensaver
+sudo apt install software-properties-common build-essential make vim git curl libx11-dev python-dev libxft-dev sshfs libevent-dev libncurses-dev rofi ibus-rime ibus-gtk ibus-gtk3 libx11-dev python-dev libxft-dev lxrandr sshfs
 
-ranger --copy-config all
+
+
+sudo apt-add-repository ppa:fish-shell/release-3
+sudo apt-get update
+sudo apt-get install fish
+mkdir -p ~/.config/fish
+cp ~/dotfiles/config.fish ~/.config/fish/config.fish
+
+
+
 
 cd ~/Downloads
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 sudo apt -f install
 
-git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
 
-cd ~/Downloads
+
+
+TMUX_VERSION=3.0
+
+wget https://github.com/tmux/tmux/releases/download/${TMUX_VERSION}/tmux-${TMUX_VERSION}.tar.gz
+tar xf tmux-${TMUX_VERSION}.tar.gz
+rm -f tmux-${TMUX_VERSION}.tar.gz
+
+cd tmux-${TMUX_VERSION}
+./configure
+make
+sudo make install
+cd -
+sudo rm -rf /usr/local/src/tmux-\*
+sudo mv tmux-${TMUX_VERSION} /usr/local/src
+
+
+
+
 curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > install.sh
 sh ./install.sh
+rm install.sh
+
+
+git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
 mkdir -p ~/.vim/colors
 cd ~/.vim/colors
 git clone git://github.com/chriskempson/base16-vim.git base16
 cp base16/colors/*.vim .
 cp ~/dotfiles/vimrc ~/.vimrc
 
-# sudo add-apt-repository ppa:linrunner/tlp
-# sudo apt update
-# sudo apt install tlp 
-# sudo apt install tlp-rdw
-# sudo apt install tp-smapi-dkms acpi-call-dkms
-# sudo apt install acpitool
-# sudo tlp start
-# sudo apt install powertop
-# sudo powertop --auto-tune
-
-mkdir -p ~/.config/fish
-cp ~/dotfiles/config.fish ~/.config/fish/config.fish
-curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs https://git.io/fisher
-fisher rafaelrinaldi/pure
-fisher z
-
-cp -r ~/dotfiles/awesome ~/.config/awesome
-cd ~/.config/awesome
-git clone https://github.com/deficient/volume-control.git
-git clone https://github.com/deficient/battery-widget.git
-sudo systemctl enable acpid
 
 git config --global user.email "shifeng@cs.umd.edu"
 git config --global user.name "ihsgnef"
@@ -54,14 +57,3 @@ git config --global alias.br branch
 git config --global alias.ci commit
 git config --global alias.st status
 git config --global push.default simple
-
-cp ~/dotfiles/tmux.conf ~/.tmux.conf
-
-cd ~/Downloads
-wget 'https://dl.suckless.org/st/st-0.8.1.tar.gz'
-tar -xzvf st-0.8.1.tar.gz
-cd ~/Downloads/st-0.8.1/
-sudo make clean install
-
-git clone https://github.com/newhoa/victory-icon-theme.git ~/.icons/victory-icon-theme/
-git clone https://github.com/serialoverflow/awesome-themes.git ~/.config/awesome/themes/
