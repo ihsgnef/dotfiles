877b49aa1aa694b4b959927e5975197db38cb99e
ihsgnef <shifeng@cs.umd.edu>
2020-03-07 18:04:45 -0500

    add ulauncher

--- setup_thinkpad.sh
+++ setup_thinkpad.sh
@@ -57,3 +57,9 @@ git config --global alias.br branch
 git config --global alias.ci commit
 git config --global alias.st status
 git config --global push.default simple
+
+
+
+sudo apt-add-repository ppa:agornostal/ulauncher
+sudo apt-get update
+sudo apt-get install ulauncher
