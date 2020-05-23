d6d68f25ab50ab73fc270dd08f5f3feebeb3aa8f
ihsgnef <shifeng@cs.umd.edu>
2020-05-23 16:09:28 -0400

    update

--- setup_linux.sh
+++ setup_linux.sh
@@ -3,38 +3,23 @@ sudo apt update
 sudo apt -y install software-properties-common build-essential make vim git curl libx11-dev python-dev libxft-dev libevent-dev libncurses-dev lxrandr sshfs
 
 
+# # brew
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+
+
 # ssh
 cp ~/dotfiles/ssh_config ~/.ssh/config
-ssh-copy-id clipsub
+# ssh-copy-id clipsub
 
 
-# fish
-sudo apt-add-repository -y ppa:fish-shell/release-3
-sudo apt update
-sudo apt -y install fish
+# fish config
 mkdir -p ~/.config/fish
 cp ~/dotfiles/config.fish ~/.config/fish/config.fish
 
 
-# chrome
-cd ~/Downloads
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
-sudo apt -f install
 
-
-# tmux
-TMUX_VERSION=3.0
-wget https://github.com/tmux/tmux/releases/download/${TMUX_VERSION}/tmux-${TMUX_VERSION}.tar.gz
-tar xf tmux-${TMUX_VERSION}.tar.gz
-rm -f tmux-${TMUX_VERSION}.tar.gz
-cd tmux-${TMUX_VERSION}
-./configure
-make
-sudo make install
-cd -
-sudo rm -rf /usr/local/src/tmux-\*
-sudo mv tmux-${TMUX_VERSION} /usr/local/src
+# tmux config
 cp ~/dotfiles/tmux.conf ~/.tmux.conf
 
 
@@ -61,9 +46,3 @@ git config --global alias.br branch
 git config --global alias.ci commit
 git config --global alias.st status
 git config --global push.default simple
-
-
-# ulauncher
-sudo apt-add-repository -y ppa:agornostal/ulauncher
-sudo apt update
-sudo apt -y install ulauncher
